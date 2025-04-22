312f8929be0ca52a906456876426419356b289df
Caio Massote <52792957+Thecaiobr@users.noreply.github.com>
2025-04-22 11:40:00 -0300

    Add files via upload

---
+++ countAndSay.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} n
+ * @return {string}
+ */
+/*
+38. Count and Say
+The count-and-say sequence is a sequence of digit strings defined by the recursive formula:
+countAndSay(1) = "1"
+countAndSay(n) is the run-length encoding of countAndSay(n - 1).
+Run-length encoding (RLE) is a string compression method that works by replacing consecutive identical characters (repeated 2 or more times)
+with the concatenation of the character and the number marking the count of the characters (length of the run).
+For example, to compress the string "3322251" we replace "33" with "23", replace "222" with "32", replace "5" with "15" and replace "1" with "11".
+Thus the compressed string becomes "23321511".
+Given a positive integer n, return the nth element of the count-and-say sequence.
+*/
+var countAndSay = function(n) {
+    let res = "1";
+    for(let i = 1; i < n;i++){
+        res = rle(res)
+    }
+    return res;
+};
+
+function rle(s){
+    let result = "";
+    let count = 1;
+    for(let i = 1; i < s.length; i++){
+        if(s[i] === s[i - 1]){
+            count++
+        }else{
+            result += count + s[i - 1]
+            count = 1
+        }
+    }
+    result += count + s[s.length - 1]
+    return result
+}
+
